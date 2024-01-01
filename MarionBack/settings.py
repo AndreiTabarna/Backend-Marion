a66e9eb7f22646d67cbfeebaebbf58d1f1385104
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-01 14:57:04 +0200

    Update settings.py

--- MarionBack/settings.py
+++ MarionBack/settings.py
@@ -84,6 +84,12 @@ WSGI_APPLICATION = 'MarionBack.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgres://marionturism:marionturism@localhost:5432/marionturism'),
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
