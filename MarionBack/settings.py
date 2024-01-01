6ffe49c02b22aa74934179f9d5864e313f747c19
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-01 16:23:28 +0200

    Update settings.py

--- MarionBack/settings.py
+++ MarionBack/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '46962',
+        'PORT': '47535',
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'NAME': os.environ.get('POSTGRES_DB'),
