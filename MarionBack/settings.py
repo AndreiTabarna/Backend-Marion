1adc319bdcd93c22cfe87ce5715fe614a6e25631
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-02 15:46:15 +0200

    Update settings.py

--- MarionBack/settings.py
+++ MarionBack/settings.py
@@ -58,7 +58,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Adaugă adresa de la care servești aplicația React
+    "http://localhost:3000",
+    "https://frontend-marion-production.up.railway.app/",
 ]
 
 ROOT_URLCONF = 'MarionBack.urls'
