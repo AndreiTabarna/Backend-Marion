8fadb60d259c1637b52f3e2ce7866b26831428eb
AndreiTabarna <100340254+AndreiTabarna@users.noreply.github.com>
2024-01-03 14:25:57 +0200

    nume bd

--- Back/admin.py
+++ Back/admin.py
@@ -1,7 +1,18 @@
-# admin.py
-
 from django.contrib import admin
 from .models import Element
 
-admin.site.register(Element)
+class ElementAdmin(admin.ModelAdmin):
+    list_display = ('get_element_name',)
+
+    def get_element_name(self, obj):
+        # Extrage numele dintre primul și al doilea caracter "/"
+        if '/' in obj.url:
+            start_index = obj.url.index('/') + 1
+            end_index = obj.url.index('/', start_index)
+            return obj.url[start_index:end_index]
+        return obj.url  # Dacă nu există "/" sau obiectul nu are un nume între "/", afișează întregul URL
+
+    get_element_name.short_description = 'Nume Element'
+
+admin.site.register(Element, ElementAdmin)
 
